cdb69078e19a5177e2cbd529e5498c130c440c21
Andrew <andrew.spon@gmail.com>
2021-10-25 20:09:20 +0000

    adding assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅✅✅ Assertion Passed!");
+  } else {
+    console.log("❌❌❌Assertion Failed!");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1,1);
+
