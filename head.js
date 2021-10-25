c43dbdee0a2673b7e65b2d3fa8e1f16fbc78d7a3
Andrew <andrew.spon@gmail.com>
2021-10-25 22:10:29 +0000

    eslint updates

--- head.js
+++ head.js
@@ -6,11 +6,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-let head = function (actual) {
-  return actual[0]
+let head = function(actual) {
+  return actual[0];
 };
 
 assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Lighthouse", "Labs", "Hello"]), "Lighthouse")
+assertEqual(head(["Lighthouse", "Labs", "Hello"]), "Lighthouse");
 assertEqual(head([]), 6);
 assertEqual(head([6]), 6);
