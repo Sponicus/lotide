cbcf88f24db5177de3cbe01e89655f5f076e2dd2
Andrew <andrew.spon@gmail.com>
2021-10-28 02:57:28 +0000

    remove excess coments

--- letterPositions.js
+++ letterPositions.js
@@ -17,28 +17,15 @@ const eqArrays = function(array1,array2) {
 
 const letterPositions = function(sentence) {
   const results = {};
-  // loop through sentence. 
   for(let i = 0; i < sentence.length; i++) {
-    /*console.log(i);
-    console.log(sentence[i]);*/
-    //add each letter to a key in results and add their position to the key as part of an array. Make sure no spaces are added to the object
     if (sentence[i] === " ") {
       //do nothing
     } else if (results[sentence[i]] !== undefined) {
-      //add position to key
       results[sentence[i]].push(i);
-      //console.log(i);
     } else {
-      //add letter and positiom
       results[sentence[i]] = [i]
-      //console.log(sentence[i]);
     }   
   }
-  //console.log(results)
-  //return said object with results 
   return results;
 };
-
-///// Test /////
-//letterPositions('hello dear')
 assertArraysEquals(letterPositions("hello").e, [1]);
