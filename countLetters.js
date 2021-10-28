cad9a69e913e393351fac0fc3c867b88ca99b099
Andrew <andrew.spon@gmail.com>
2021-10-28 01:25:12 +0000

    fix linting

--- countLetters.js
+++ countLetters.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   /// with Mentor has to adjust assertEqual function for Object
   let result = true;
-  for(let t in actual){
-    if(actual[t]!==expected[t]){
+  for (let t in actual) {
+    if (actual[t] !== expected[t]) {
       result = false;
     }
   }
@@ -19,11 +19,11 @@ const assertEqual = function(actual, expected) {
 
 const countLetters = function(allLetters) {
   let results = {};
-  // loop through string 
+  // loop through string
   for (let letter of allLetters) {
     //IF it is first time add letter to object and ommit spaces and count all letters
     if (letter === " ") {
-    } else if(results[letter] > 0) {
+    } else if (results[letter] > 0) {
       results[letter] += 1;
     } else {
       results[letter] = 1;
@@ -38,4 +38,4 @@ const countLetters = function(allLetters) {
 //console.log(countLetters('hello'));
 assertEqual(countLetters('rohit'), { r: 1, o: 1, h: 1, i: 1, t: 1 });
 assertEqual(countLetters('hello'), { h: 1, e: 1, l: 2, o: 1 });
-assertEqual(countLetters("a walk in the park"), { a: 3, w: 1, l: 1, k: 2, i: 1, n: 1, t: 1, h: 1, e: 1, p: 1, r: 1})
+assertEqual(countLetters("a walk in the park"), { a: 3, w: 1, l: 1, k: 2, i: 1, n: 1, t: 1, h: 1, e: 1, p: 1, r: 1});
