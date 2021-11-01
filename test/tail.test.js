1c43df85c3cfd612bad1696659e6619330ee00c4
Andrew <andrew.spon@gmail.com>
2021-11-01 19:01:52 +0000

    refactor tail.test for mocha && chai

--- test/tail.test.js
+++ test/tail.test.js
@@ -1,8 +1,27 @@
-const assertEqual = require('../assertEqual');
+// const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const tail = require('../tail');
+
+
+describe('#tail', () => {
+
+  it('return 3 for words.length', () => {
+    assert.deepEqual(words.length, 3);
+  });
+
+  it('return 1 for words.slice(2)', () => {
+    assert.deepEqual(words.slice(2).length,1);
+  });
+
+  it('return 2 for words.slice(1)', () => {
+    assert.deepEqual(words.slice(1).length, 2);
+  });
+});
 
 const words = ["Yo Yo", "Lighthouse", "Labs"];
 tail(words);
-assertEqual(words.length, 3);
-assertEqual(words.slice(2).length, 1);
-assertEqual(words.slice(1).length,2);
+// const words = ["Yo Yo", "Lighthouse", "Labs"];
+// tail(words);
+// assertEqual(words.length, 3);
+// assertEqual(words.slice(2).length, 1);
+// assertEqual(words.slice(1).length, 2);  
